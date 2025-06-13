0126d833989a4bd17f3564745f2c767341de9b87
Pjh <lucete331@naver.com>
2025-06-13 17:20:03 +0900

    pjh commit

--- fastapi-ml/chartTest.py
+++ fastapi-ml/chartTest.py
@@ -7,48 +7,49 @@ plt.rcParams['font.family'] = 'Gulim'
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_excel("data/가스공급 전처리 데이터.xlsx", sheet_name='2023')
-xls = pd.read_excel("data/가스공급 전처리 데이터.xlsx", sheet_name=None)
+df = pd.read_excel("./data/GasSupplyData.xlsx", sheet_name='2023')
+xls = pd.read_excel("./data/GasSupplyData.xlsx", sheet_name=None)
 
 # 각 시트별 데이터프레임을 리스트로 저장
 df_list = []
 
 for sheet_name, df in xls.items():
-    df['연도'] = int(sheet_name)  # 시트명이 '2020', '2021' 이런 식이라고 가정
+    df['Year'] = int(sheet_name)  # 시트명이 '2020', '2021' 이런 식이라고 가정
     df.fillna(0, inplace=True)
     df_list.append(df)
 
 # 하나로 합치기
 df_all = pd.concat(df_list, ignore_index=True)
 
-df_all.fillna(0, inplace=True)
 # df.replace({'지역':{'전국':0, '서울':1, '인천':2, '경기':3, '부산':4, '대구':5, '광주':6,
 #                   '대전':7, '울산':8, '세종':9, '강원':10, '충북':11, '충남':12, '전북':13,
 #                   '전남':14, '경북':15, '경남':16, '제주':17,}}, inplace=True)
 
-df_seoul = df_all[df_all['지역'] == '서울']
-sns.barplot(x='월', y='공급량', hue='지역', data=df)
+df_all['Month'] = df_all['Month'].astype(int)
+
+df_seoul = df_all[df_all['Local'] == '서울']
+sns.barplot(x='Month', y='Supply', hue='Year', data=df_seoul)
 plt.show()
 
 
-df2=pd.read_excel("data/기상청 전처리 데이터.xlsx", sheet_name='서울_전처리')
+df2=pd.read_excel("./data/WeatherData.xlsx", sheet_name='서울_전처리')
 df2.fillna(0, inplace=True)
 
-df2_2023 = df2[df2['년'] == 2023]
+df2_2023 = df2[df2['Year'] == 2023]
 # sns.barplot(x='월', y='평균기온', data=df2_2023)
 # plt.show()
 
 # 월별로 정렬 (혹시 월 순서가 안 맞을 때 대비)
-df_seoul = df_seoul.sort_values('월')
-df2_2023 = df2_2023.sort_values('월')
+df_seoul = df_seoul.sort_values('Month')
+df2_2023 = df2_2023.sort_values('Month')
 
 # 필요한 컬럼만 뽑아서 데이터프레임 만들기
-merged = pd.merge(df_seoul[['월', '공급량']],
-                  df2_2023[['월', '평균기온']],
-                  on='월')
+merged = pd.merge(df_seoul[['Month', 'Supply']],
+                  df2_2023[['Month', 'AvgTa']],
+                  on='Month')
 
 # 상관계수 계산 (피어슨)
-correlation = merged['공급량'].corr(merged['평균기온'])
+correlation = merged['Supply'].corr(merged['AvgTa'])
 # print(f"공급량과 평균기온 간 상관계수: {correlation:.3f}")
 
 # 그래프
