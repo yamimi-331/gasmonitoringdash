a2d03d872c39135fd08e11396e580ea2552f04cb
Donggu6 <dlehdrjs0101@gmail.com>
2025-06-17 17:46:22 +0900

    ldg commit main py fix

--- fastapi-ml/main.py
+++ fastapi-ml/main.py
@@ -6,11 +6,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pandas as pd
 import numpy as np
-import json
-# from gas_prediction import get_prediction_results
-from pjh import get_local_result
-from functionSett import get_total_supply_by_region_2025
-from shj import plot_supply_prediction_timeline_A,load_and_preprocess_data
 
 from prediction.prediction_lstm import get_prediction_json
 from prediction.prediction_xgboost import prediction_xgboost
@@ -43,50 +38,6 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
-
-# @app.get("/api/gas/prediction")
-# async def get_gas_prediction():
-#     try:
-#         results = get_prediction_results()
-#         return results
-#     except Exception as e:
-#         return {"error": str(e)}
-    
-@app.get("/api/gas/prediction")
-async def get_local(city: str = Query(None)):
-    try:
-        results = get_local_result(city)
-        return results
-    except Exception as e:
-        return {"error": str(e)}
-    
-@app.get("/api/gas/local")
-async def get_local(city: str = Query(None), model: str = Query(None), period: int = Query(3)):
-    try:
-        results = get_local_result(city, model, period)
-        return results
-    except Exception as e:
-        return {"error": str(e)}
-    
-
-@app.get("/api/gas/supply/2025")
-async def get_supply_by_region_2025():
-    try:
-        result = get_total_supply_by_region_2025()
-        return result
-    except Exception as e:
-        return {"error": str(e)}
-    
-@app.get("/api-supply-prediction")
-async def get_supply_prediction():
-    try:
-        df = load_and_preprocess_data()
-        results = plot_supply_prediction_timeline_A(df, 'xgboost', start_date='2024-01-01', end_date='2025-07-01')
-        return JSONResponse(content=results)
-    except Exception as e:
-        return {"error": str(e)}
-    
-
 
 @app.get("/api/gas/lstm-prediction")
 async def get_lstm_prediction(
