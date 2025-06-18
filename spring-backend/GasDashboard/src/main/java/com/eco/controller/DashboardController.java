2a1c771a119127796798e497cdd1189dc9e1a3cc
Donggu6 <dlehdrjs0101@gmail.com>
2025-06-18 16:55:20 +0900

    ldg dashboard rename

--- spring-backend/GasDashboard/src/main/java/com/eco/controller/DashboardController.java
+++ spring-backend/GasDashboard/src/main/java/com/eco/controller/DashboardController.java
@@ -8,30 +8,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@Log4j
 @RequestMapping("/Dashboard")
 public class DashboardController {
-	private final RestTemplate restTemplate = new RestTemplate();
-	private final String fastApiUrl = "http://localhost:8000/api/gas/prediction";
+//	private final RestTemplate restTemplate = new RestTemplate();
+//	private final String fastApiUrl = "http://localhost:8000/api/gas/prediction";
 
 	// Dashboard 페이지로 이동
 	// FastAPI에 데이터를 요청하여 받은 응답기반으로 Dashboard 페이지로 이동 하면서 model에 정보 담아 전달하는 함수
 	@GetMapping("")
 	public String getGasPrediction(Model model) {
-		try {
-			// FastAPI 응답 JSON을 Map 형태로 파싱
-			Map<String, Object> response = restTemplate.getForObject(fastApiUrl, Map.class);
-
-			if (response != null && response.containsKey("models") && response.containsKey("visualizations")) {
-				model.addAttribute("models", response.get("models"));
-				model.addAttribute("visualizations", response.get("visualizations"));
-			} else {
-				model.addAttribute("error", "FastAPI 응답에 데이터가 없습니다.");
-			}
-		} catch (Exception e) {
-			model.addAttribute("error", "FastAPI 요청 실패: " + e.getMessage());
-		}
-
-		return "userDashboard";
+//		try {
+//			// FastAPI 응답 JSON을 Map 형태로 파싱
+//			Map<String, Object> response = restTemplate.getForObject(fastApiUrl, Map.class);
+//
+//			if (response != null && response.containsKey("models") && response.containsKey("visualizations")) {
+//				model.addAttribute("models", response.get("models"));
+//				model.addAttribute("visualizations", response.get("visualizations"));
+//			} else {
+//				model.addAttribute("error", "FastAPI 응답에 데이터가 없습니다.");
+//			}
+//		} catch (Exception e) {
+//			model.addAttribute("error", "FastAPI 요청 실패: " + e.getMessage());
+//		}
+		log.info("대시보드 페이지로 이동");
+		return "dashboard";
 	}
 }
