04473fcbefa6677fed64a52dca7c85b59877292d
Pjh <lucete331@naver.com>
2025-06-19 14:40:01 +0900

    pjh sql insert

---
+++ mysql-query/t_local_insert.sql
@@ -0,0 +1,21 @@
+use gasdashboarddb;
+
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L01'	,	'서울특별시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L02'	,	'인천광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L03'	,	'경기도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L04'	,	'부산광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L05'	,	'대구광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L06'	,	'광주광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L07'	,	'대전광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L08'	,	'울산광역시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L09'	,	'세종특별 자치시'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L10'	,	'강원특별자치도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L11'	,	'충청북도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L12'	,	'충청남도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L13'	,	'전북특별자치도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L14'	,	'전라남도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L15'	,	'경상북도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L16'	,	'경상남도'	);
+INSERT INTO T_LOCAL(local_cd, local_nm) VALUES(	'L17'	,	'제주특별자치도'	);
+
+select * from t_local;
