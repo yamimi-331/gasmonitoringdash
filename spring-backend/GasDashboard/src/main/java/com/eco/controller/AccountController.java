89597ddf66268315412046408c2d9198f1cda4a6
Pjh <lucete331@naver.com>
2025-06-23 10:34:16 +0900

    pjh 관리자 권한 요청자 목록 출력

--- spring-backend/GasDashboard/src/main/java/com/eco/controller/AccountController.java
+++ spring-backend/GasDashboard/src/main/java/com/eco/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.eco.controller;
 
+import java.util.List; 
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,17 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eco.domain.UserVO;
+import com.eco.service.AdminService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
+@AllArgsConstructor
 @RequestMapping("/account")
 public class AccountController {
+	private AdminService adminService;
 	
 	// 사용자 계정 관리 페이지로 이동
 	@GetMapping("")
-	public String adminPage(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+	public String accountManagePage(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
 		UserVO user = (UserVO) session.getAttribute("currentUserInfo");
 		if (user == null) {
 			return "redirect:/login";
@@ -28,6 +34,14 @@ public class AccountController {
 		String userType = user.getUser_type();
 		if ("admin".equals(userType)) {
 			model.addAttribute("currentUserInfo", user);
+			
+			//관리자 권한 요청 계정 조회
+			List<UserVO> manageUser = adminService.searchPreAccount();
+			model.addAttribute("manageUser", manageUser);
+			if (manageUser.isEmpty()) {
+				model.addAttribute("accountManageMsg", "권한 요청한 사용자가 없습니다.");
+			}
+			
 			return "account";
 		} else {
 			redirectAttributes.addFlashAttribute("msg", "관리자 계정만 접근 가능합니다.");
--- spring-backend/GasDashboard/src/main/webapp/WEB-INF/views/account.jsp
+++ spring-backend/GasDashboard/src/main/webapp/WEB-INF/views/account.jsp
@@ -8,6 +8,36 @@
 <title>Insert title here</title>
 </head>
 <body>
-	사용자 계정 관리
+<table>
+	<colgroup>
+		<col width="25%">
+		<col width="25%">
+		<col width="25%">
+		<col width="25%">
+	</colgroup>
+	<tr>
+		<th>아이디</th>
+		<th>이름</th>
+		<th>현재 등급</th>
+		<th>승인 버튼</th>
+	</tr>
+	<c:choose>
+		<c:when test="${not empty manageUser}">
+			<c:forEach var="item" items="${manageUser}">
+				<tr>
+					<td>${item.user_id}</td>
+					<td>${item.user_nm}</td>
+					<td>${item.user_type}</td>
+					<td><button>승인</button></td>
+				</tr>
+			</c:forEach>
+		</c:when>
+		<c:otherwise>
+			<tr>
+				<td colspan="4">${ accountManageMsg }</td>
+			</tr>
+		</c:otherwise>
+	</c:choose>
+</table>
 </body>
 </html>
